9874242b4a79bbe7a6e64279f9f3cf9ac635cead
Esteban-bit-dot <eandrescardales@gmail.com>
2025-02-24 15:21:37 -0500

    Create Juego_FizzBuzz.java

---
+++ Juego_FizzBuzz.java
@@ -0,0 +1,27 @@
+package ejercicios.poo;
+
+public class Juego_FizzBuzz {
+    public class FizzBuzz {
+    public String calcular(int numero) {
+        if (numero % 3 == 0 && numero % 5 == 0) {
+            return "FizzBuzz";
+        } else if (numero % 3 == 0) {
+            return "Fizz";
+        } else if (numero % 5 == 0) {
+            return "Buzz";
+        } else {
+            return Integer.toString(numero);
+        }
+    }
+}
+public class Principal {
+    public static void main(String datofizzBuzz) {
+        FizzBuzz datofizzBuzz = new FizzBuzz();
+        
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(datofizzBuzz.calcular(i));
+        }
+    }
+}
+
+}
