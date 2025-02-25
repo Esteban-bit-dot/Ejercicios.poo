b5862d74b165a68b8e5949a23a52ebeb4873b988
Esteban-bit-dot <eandrescardales@gmail.com>
2025-02-25 06:33:47 -0500

    Create Principal.java

---
+++ Principal.java
@@ -0,0 +1,20 @@
+    public class principal {
+
+    public static void main(String[] args) {
+         Scanner Tomardatos = new Scanner(System.in);
+         Sala datosala = new Sala(10); // Creando una sala con 10 asientos
+        Usuario datoUsuario = new Usuario("");
+        
+        while (Sala.hayAsientosDisponibles()) {
+            System.out.println("¿Desea reservar un asiento? (s/n)");
+            String respuesta = Tomardatos.nextLine();
+            if (respuesta.equalsIgnoreCase("s")) {
+                datoUsuario.reservarAsiento(Sala);
+            } else {
+                break;
+            }
+        }
+        System.out.println("No hay más asientos disponibles o ha decidido no reservar más asientos.");
+        Tomardatos.close();
+      }
+    }
