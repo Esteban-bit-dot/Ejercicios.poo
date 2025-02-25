8193d49e0466a4e3856d212e05d685e26f9fd003
Esteban-bit-dot <eandrescardales@gmail.com>
2025-02-25 06:30:16 -0500

    Update Reserva.java

--- Reserva.java
+++ Reserva.java
@@ -26,33 +26,7 @@ public class Reserva {
         Tomardatos.close();
       }
     }
-public class Persona {
-      
-   private static String Nombre;
-    private static String Correo;
-    private int Telefono;
-    
-    public Persona(String datoNombre, String datoCorreo, int datoTelefono){
-        Persona.Nombre = datoNombre;
-        Persona.Correo = datoCorreo;
-        this.Telefono = datoTelefono;
-    }
-    public int getTelefono(){
-        int auxTelefono = this.Telefono;
-       return auxTelefono;
-    }
-    public void setTelefono(int datoTelefono){
-       this.Telefono = datoTelefono;
-    }
-    public void imprimirdatos(){
-        System.out.println("El Nombre es: "+Persona.Nombre);
-        System.out.println("El Correo es: "+Persona.Correo);
-    }
-    public static void telefono(){
-        Persona objtelefono = new Telefono("");
-        objtelefono.imprimirdatos();
-        System.out.println("el telefono es: "+Persona.Telefono);
-    }
+
     
    
 public class Sala {
