d88b3afb25a8765f211c0b5cde0d043ddbd9339c
Esteban-bit-dot <eandrescardales@gmail.com>
2025-02-25 09:14:54 -0500

    Create Aire_Acondicionado.java

---
+++ Aire_Acondicionado.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+
+public class Aire_Acondicionado {
+    
+    public  static void temperatura (){
+        Scanner Temperatura = new Scanner(System.in);
+        Objtemp.temperatura ="Aire";
+        int temp;
+        System.out.print("ingrese la temperatura c°: ");
+        if (temp>28°) {
+            System.out.println("Activado");
+        }else{
+            System.out.println("desactivado");
+        }
+        
+    }
+    
+   public static void humedad(){
+       int humedad;
+        objtemp.humedad="Aire";
+       if (humedad>60%) {
+           System.out.println("Activado");
+       }else{
+           System.out.print("desactivado");
+       }
+   }
+   public static void sensor(){
+       boolean = true;
+       while (tem>30°=humdad){
+       if(true){
+       System.out.print("luz verde encendido");
+       }else(false){
+       System.out.print("luz roja apagado");
+       }
+      break;
+   }
+         
+   } 
+}
