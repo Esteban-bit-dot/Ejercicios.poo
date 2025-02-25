2a43784441e716b09556c2dc08fdcab39bcb13ee
Esteban-bit-dot <eandrescardales@gmail.com>
2025-02-25 06:34:54 -0500

    Update Reserva.java

--- Reserva.java
+++ Reserva.java
@@ -7,19 +7,6 @@ import java.util.Scanner;
 public class Reserva {
     
 
-
-    
-   
-public class Sala {
-    private ArrayList<Asiento> Asientos;
-    
-    public Sala(int capacidad) {
-        Asientos = new ArrayList<>(capacidad);
-        for (int i = 0; i < capacidad; i++) {
-            Asientos.add(new Asiento());
-        }
-    }
-    
     public boolean hayAsientosDisponibles() {
         for (Asiento datoasiento : Asientos) {
             if (datoasiento.isDisponible()) {
