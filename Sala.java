861a35e9a410714398c8598445735e198b2bad63
Esteban-bit-dot <eandrescardales@gmail.com>
2025-02-25 06:35:41 -0500

    Create Sala.java

---
+++ Sala.java
@@ -0,0 +1,10 @@
+public class Sala {
+    private ArrayList<Asiento> Asientos;
+    
+    public Sala(int capacidad) {
+        Asientos = new ArrayList<>(capacidad);
+        for (int i = 0; i < capacidad; i++) {
+            Asientos.add(new Asiento());
+        }
+    }
+    
