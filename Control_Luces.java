7774b0e79d84bd9e153376c7e9585dcffdb11f07
Esteban-bit-dot <eandrescardales@gmail.com>
2025-02-25 08:24:20 -0500

    Create Control_Luces.java

---
+++ Control_Luces.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class control_Luces {
+    
+    public static void prenderluces(){
+        Scanner luceScanner = new Scanner(System.in);
+        noche objnoche = new noche();
+        prenderluces();
+        if (prenderluces=noche){
+            boolean = true;
+           System.out.println("luces prendidas");
+        }else(false){
+          System.out.println("luces apagadas");
+        }
+    }
+    public static void movimiento(){
+        movimiento objmovimiento = new movimiento();
+        movimiento();
+        if (prenderluces=movimiento){
+          boolean = true;
+           System.out.println("luces prendidas"); 
+        }else(false){
+           System.out.println("luces apagadas"); 
+        }
+    }
+     public static void Apagarluces(){
+       dia objdia =  new dia();
+       Apagarluces();
+      while(luces){
+        if (dia=luces){
+            System.out.println("luces apagadas");
+            boolean = true;
+            break;
+        }else(false){
+            System.out.println("luces prendidas");
+        }
+      }
+   }
+}
