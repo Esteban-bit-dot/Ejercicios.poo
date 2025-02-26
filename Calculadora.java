947b86bc3babd5a9103cdfb0396d1ae57677334a
Esteban-bit-dot <eandrescardales@gmail.com>
2025-02-26 08:12:51 -0500

    Update Calculadora.java

--- Calculadora.java
+++ Calculadora.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 
 public class Calculadora {
     
-
 public class Principal {
     public static void main(String[] args) {
         Scanner Tomardatos = new Scanner(System.in);
