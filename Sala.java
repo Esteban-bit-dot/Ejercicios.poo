44863ab52ef12673c2b6354adc7a3e2d3612d9f3
Esteban-bit-dot <eandrescardales@gmail.com>
2025-02-26 07:53:42 -0500

    Update Sala.java

--- Sala.java
+++ Sala.java
@@ -3,6 +3,7 @@ public class Sala {
     private ArrayList<Asiento> Asientos;
     
     public Sala(int capacidad) {
+        Cantidad Asientos = "100";
         Asientos = new ArrayList<>(capacidad);
         for (int i = 0; i < capacidad; i++) {
             Asientos.add(new Asiento());
