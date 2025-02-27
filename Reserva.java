11e8e7d5c7b7d89b9ad3365e41522292a929252b
Esteban-bit-dot <eandrescardales@gmail.com>
2025-02-27 08:50:45 -0500

    Update Reserva.java

--- Reserva.java
+++ Reserva.java
@@ -2,6 +2,7 @@
 public class Reserva {
     
     public void reservarAsiento() {
+        objAsiento reservar = datoSala();
         for (Asiento datoAsiento : Asientos) {
             if (Asiento.isDisponible()) {
                 Asiento.reservar();
