c6634df29494577e64d8792b0e6b85a34ab4166a
Esteban-bit-dot <eandrescardales@gmail.com>
2025-02-27 08:55:49 -0500

    Update usuario.java

--- usuario.java
+++ usuario.java
@@ -3,6 +3,20 @@
     public Usuario(String datonombre, String datoCorreo, int datocontraceña) {
         super(datonombre, datoCorreo, datocontraceña);
     }
+        Scanner objTeclado = new Scanner(System.in);
+        Usuario objUsuario = new Usuario();
+          
+       System.out.println("Digite el nombre");
+        String datoNombre = objteclado.nextLine();
+        objUsuario.setNombreUsuario(datoNombre);
+
+        System.out.println("Digite el Correo");
+        String datoCorreo= objteclado.nextLine();
+        objUsuario.setCorreoUsuario(datoCorreo);
+
+       System.out.println("Digite la Contraseña");
+        String datoContraseña= objteclado.nextLine();
+        objUsuario.setCorreoUsuario(datoContraseña);
   
     public void reservarAsiento(String datosala) {
         if (datosala.hayAsientosDisponibles()) {
