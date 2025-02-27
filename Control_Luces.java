c17af4c9e0acaec20bc7b890ad0a8752ac98e3d8
Esteban-bit-dot <eandrescardales@gmail.com>
2025-02-27 09:01:13 -0500

    Update Control_Luces.java

--- Control_Luces.java
+++ Control_Luces.java
@@ -25,7 +25,7 @@ public class control_Luces {
     }
      public static void Apagarluces(){
        dia objdia =  new dia();
-       Apagarluces();
+       objApagarluces.setdia(datodia);
       while(luces){
         if (dia=luces){
             System.out.println("luces apagadas");
