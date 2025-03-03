add337b36d92ab6b79857fc956f92d8b2f1edb13
Esteban-bit-dot <eandrescardales@gmail.com>
2025-03-03 06:39:49 -0500

    Update Control_Luces.java

--- Control_Luces.java
+++ Control_Luces.java
@@ -5,6 +5,7 @@ public class control_Luces {
     public static void prenderluces(){
         Scanner luceScanner = new Scanner(System.in);
         noche objnoche = new noche();
+        objCasa Sensor = new luces.nextInt();
         objprenderluces.setnocheControl_luces(datonoche);
         if (prenderluces=noche){
             boolean = true;
@@ -15,6 +16,7 @@ public class control_Luces {
     }
     public static void movimiento(){
         movimiento objmovimiento = new movimiento();
+        objCasa Sensor = new luces.nextInt();
         objmovimiento.setmovimientoControl_luces(datomovimiento);
         if (prenderluces = movimiento){
           boolean = true;
@@ -26,6 +28,7 @@ public class control_Luces {
      public static void Apagarluces(){
        dia objdia =  new dia();
        objApagarluces.setdiaControl_luces(datodia);
+       objCasa Sensor = new luces.nextInt();
       while(luces){
         if (dia=luces){
             System.out.println("luces apagadas");
